85f5841af131f2dda7917de0b47ba4c88c7cf43d
hppsrc <horahenaripo+new@gmail.com>
2025-01-21 20:14:52 -0500

    0.1.1-alpha
    - Simple text update.

--- src/public/index.js
+++ src/public/index.js
@@ -1,15 +1,15 @@
-const version = "0.1.0-alpha";
-const build = "2501201742";
+const version = "0.1.1-alpha";
+const build = "250121141";
 const git_branch = "dev";
 
 // header
 let header = document.createElement('header');
 header.classList = "glow"
-header.innerHTML = " <h1> Paragraph </h1> <p> An FOSS Word processor </p> "
+header.innerHTML = `<h1> Paragraph </h1> <p> An FOSS Word processor <small style="font-size: xx-small"> Version: ${version+" ("+build+")"}</small> </p> `
 
 // main 
 const main = document.createElement('main');
-main.innerHTML = ` WIP! <br> Data: ${version+" ("+build+") (branch: "+git_branch+")"}`
+main.innerHTML = `<p>This branch doesn't contain anything functional or minimally usable.<br>If you want to try the Alpha switch to the <a href="https://github.com/hppsrc/paragraph/tree/dev" target="_blank">Dev branch</a>.</p>`
 
 // insert values
 document.body.appendChild(header);

4d38595c7b9420d897d7865cf3c03a55d874d7c3
hppsrc <horahenaripo+new@gmail.com>
2025-01-24 17:29:16 -0500

    0.1.5-alpha
    - Added more styling options and responsive to clicks, both customizable and toggleable.
    - Minor change to CSS sizes.

--- src/public/index.js
+++ src/public/index.js
@@ -1,9 +1,10 @@
-const version = "0.1.4-alpha";
-const build = "250123161";
+const version = "0.1.5-alpha";
+const build = "250124171";
 const git_branch = "dev";
 
 // vars
-const buttons = document.getElementsByClassName('action');
+const buttons_document = document.getElementsByClassName('document_action');
+const buttons_toggle = document.getElementsByClassName('document_toggle');
 
 // definitions
 menus = {
@@ -11,7 +12,7 @@ menus = {
         {
             name: "New",
             disable: false,
-            action: function() { alert("Action is not prepared yet") }
+            action: function() { location.reload() }
         },
         {
             name: "Open",
@@ -58,6 +59,16 @@ menus = {
     ]
 }
 
+menus_extra = {
+    font: [
+        "serif",
+        "sans-serif",
+        "monospace",
+        "cursive",
+        "fantasy"
+    ]
+}
+
 // 0 menu list
 // 1 set html
 
@@ -95,27 +106,34 @@ function show_action(elementId, action) {
                 } else {
 
                     div.addEventListener('click', () => {
-                        // action_box.style.display = 'none';
                         item.action()
-                        setTimeout(() => {overlay.style.opacity = "0";}, 1);
-                        setTimeout(() => {action_box.style.top = "-100px";}, 1);
-                        setTimeout(() => {action_box.innerHTML = ""}, 100);
+                        setTimeout(() => { overlay.style.opacity = "0" }, 1);
+                        setTimeout(() => { action_box.style.top = "-100px" }, 1);
+                        setTimeout(() => { action_box.innerHTML = "" }, 100);
+                        setTimeout(() => { overlay.style.display = "none"; }, 500);
                     })
                     
                 }
                 
                 action_box.appendChild(div);
+
             });
+
         }
 
         overlay.addEventListener('click', () => {
-            setTimeout(() => {overlay.style.opacity = "0";}, 1);
-            setTimeout(() => {action_box.style.top = "-100px";}, 1);
-            setTimeout(() => {action_box.innerHTML = ""}, 100);
-            overlay.removeEventListener('click');
+
+            setTimeout(() => { overlay.style.opacity = "0" }, 1);
+            setTimeout(() => { action_box.style.top = "-100px" }, 1);
+            setTimeout(() => { action_box.innerHTML = "" }, 100);
+            setTimeout(() => { overlay.style.display = "none"; }, 500);
+
         })
 
+    } else {
+
     }
+
 }
 
 // header
@@ -134,14 +152,21 @@ header.innerHTML = `
 
     <vr></vr>
 
-    <div id="header_top_bar_actions" style="f_col">
+    <div id="header_top_bar_actions" style="           ">
+    ` +                                     /* f_col */ `
         <div id="header_top_bar_actions_select">
             <p class="action" onclick="show_action(this, 0)">File</p>
             <p class="action" onclick="show_action(this, 0)">Edit</p>
         </div>
         <div id="header_top_bar_actions_do">
-            <p>Actions here...</p>
-        </div>
+            <div class="document_action" onclick="show_action(this, 1)" id="action_setfont"><p> Font: <span style="font-family: Serif"> Serif </span> </p> </div>
+            <div class="document_action" onclick="show_action(this, 1)" id="action_setsize"><p> Size: 12px </p> </div>
+            <div class="document_action" onclick="show_action(this, 1)" onclick="show_action(this, 0)" id="action_setcolorfont"><p> Font <br> </p><div id="action_setcolorfont_box"> </div> </div>
+            <div class="document_action" onclick="show_action(this, 1)" id="action_setcolorback"><p> Back <br> </p><div id="action_setcolorback_box"> </div> </div>
+            <div class="document_toggle" id="action_black"><b> N </b> </div>
+            <div class="document_toggle" id="action_italic"><i> K </i> </div>
+            <div class="document_toggle" id="action_underline"><span style="text-decoration: underline;"> U </span> </div>
+            </div>
     </div>
 
 `
@@ -158,8 +183,23 @@ action_box.id = "action_box";
 const overlay = document.createElement('div');
 overlay.id = "overlay";
 
+
 // insert values
 document.body.appendChild(action_box);
 document.body.appendChild(overlay);
 document.body.appendChild(header);
 document.body.appendChild(main);
+
+// show page
+setTimeout(() => { document.body.opacity = "0"; }, 500);
+
+// after DOM load
+for (let i = 0; i < buttons_toggle.length; i++) {
+    buttons_toggle[i].addEventListener('click', () => {
+        if (buttons_toggle[i].classList == "document_toggle clicked") {
+            buttons_toggle[i].classList = "document_toggle"
+        } else {
+            buttons_toggle[i].classList = "document_toggle clicked"
+        }
+    })
+}

4b8275ed9492bfff9afda7fd049ee50f35ecde26
hppsrc <horahenaripo+new@gmail.com>
2025-01-27 18:08:01 -0500

    0.1.6-alpha
    - Formatting was applied to the JS and CSS arhics.
    - Hidden many functions that don't work yet.
    - Added “worksheet” to the document, supports plain text.

--- src/public/index.js
+++ src/public/index.js
@@ -1,188 +1,254 @@
-const version = "0.1.5-alpha";
-const build = "250124171";
+const version = "0.1.6-alpha";
+const build = "250127181";
 const git_branch = "dev";
 
 // vars
-const buttons_document = document.getElementsByClassName('document_action');
-const buttons_toggle = document.getElementsByClassName('document_toggle');
+const buttons_document = document.getElementsByClassName("document_action");
+const buttons_toggle = document.getElementsByClassName("document_toggle");
 
 // definitions
 menus = {
-    fileMenu: [
-        {
-            name: "New",
-            disable: false,
-            action: function() { location.reload() }
-        },
-        {
-            name: "Open",
-            disable: true,
-            action : null 
-        },
-        {
-            name: "Save",
-            disable: true,
-            action : null
-        },
-        {
-            name: "Export",
-            disable: true,
-            action: null
-        }
-    ],
-    editMenu: [
-        {
-            name: "Undo",
-            disable: true,
-            action: null
-        },
-        {
-            name: "Redo",
-            disable: true,
-            action: null
-        },
-        {
-            name: "Cut",
-            disable: true,
-            action: null
-        },
-        {
-            name: "Copy",
-            disable: true,
-            action: null
-        },
-        {
-            name: "Paste",
-            disable: true,
-            action: null
-        }
-    ]
-}
+	fileMenu: [
+		{
+			name: "New file",
+			disable: false,
+			action: function () {
+				show_action(`
+					<h3>New file</h3>
+					<hr>
+					<p>Are you sure?<br>Any unsaved change will be descarted...</p>
+					<hr>
+					<div class="option_html" onclick="location.reload()">Yes</div>
+					<div class="option_html"
+					onclick="
+						setTimeout(() => { overlay.style.opacity = '0'; }, 0);
+						setTimeout(() => { overlay.style.display = 'none'; }, 100);
+						setTimeout(() => { action_box.style.top = '-500px'; }, 0);
+					"
+					>Cancel</div>
+					`, 1)
+			},
+		},
+		// {
+		// 	name: "Open",
+		// 	disable: true,
+		// 	action: null,
+		// },
+		// {
+		// 	name: "Save",
+		// 	disable: true,
+		// 	action: null,
+		// },
+		// {
+		// 	name: "Export",
+		// 	disable: true,
+		// 	action: null,
+		// },
+	],
+	editMenu: [
+		{
+			name: "Undo",
+			disable: true,
+			action: null,
+		},
+		{
+			name: "Redo",
+			disable: true,
+			action: null,
+		},
+		{
+			name: "Cut",
+			disable: true,
+			action: null,
+		},
+		{
+			name: "Copy",
+			disable: true,
+			action: null,
+		},
+		{
+			name: "Paste",
+			disable: true,
+			action: null,
+		},
+	],
+	helpMenu: [
+		{
+			name: "News",
+			disable: false,
+			action: function () {
+				show_action(`
+				<h3>News</h3>
+				<hr>
+
+				<b><p>${version}</p></b>
+				<small>- Main \"sheet\" added to the program!</small>
+
+				<hr>
+				<div class="option_html"
+				onclick="
+					setTimeout(() => { overlay.style.opacity = '0'; }, 0);
+					setTimeout(() => { overlay.style.display = 'none'; }, 100);
+					setTimeout(() => { action_box.style.top = '-500px'; }, 0);
+				"
+				>Ok!</div>
+				`,1)
+			}
+		},
+		{
+			name: "hr", disable: 0, action: 0
+		},
+		{
+			name: "About",
+			disable: false,
+			action: function () {
+				show_action(`
+				<h3>About Paragraph</h3>
+				<hr>
+				<p>Paragraph is a simple word proccesor made on JavaScript</p>
+				<b><small> Version: ${version} (${build+" "+git_branch})</small></b>
+				<hr>
+				<div class="option_html" onclick="location.reload()"><a href='https://github.com/hppsrc/paragraph'>Check Github Repo</a></div>
+				<div class="option_html"
+				onclick="
+					setTimeout(() => { overlay.style.opacity = '0'; }, 0);
+					setTimeout(() => { overlay.style.display = 'none'; }, 100);
+					setTimeout(() => { action_box.style.top = '-500px'; }, 0);
+				"
+				>Cancel</div>
+				`,1)
+			}
+		},
+	],
+};
 
 menus_extra = {
-    font: [
-        "serif",
-        "sans-serif",
-        "monospace",
-        "cursive",
-        "fantasy"
-    ]
-}
+	font: ["serif", "sans-serif", "monospace", "cursive", "fantasy"],
+};
 
 // 0 menu list
 // 1 set html
 
 // functions
-function show_action(elementId, action) {
-    
-    action_box.innerHTML = '';
+function show_action(valueArg, action) {
 
-    setTimeout(() => {action_box.style.top = "30px";}, 1);
-    
-    const rect = elementId.getBoundingClientRect();
-    
-    action_box.style.left = `${rect.x}px`
-    action_box.style.display = "flex";
-    overlay.style.display = "block";
+	if (action == 0) {
+		action_box.innerHTML = "";
 
-    setTimeout(() => {action_box.style.top = "50px";}, 1);
-    setTimeout(() => {overlay.style.opacity = "1";}, 1);
+		let current = valueArg.textContent.toLowerCase() + "Menu";
+		let values = menus[current];
 
-    if (action == 0) {
-        let current = elementId.textContent.toLowerCase() + "Menu";
-        let values = menus[current];
+		const rect = valueArg.getBoundingClientRect();
 
-        if (values) {
-            values.forEach(item => {
-                
-                let div = document.createElement('div');
-                div.textContent = item.name;
-                
-                if (item.disable) {
+		action_box.style.left = `${rect.x}px`;
+		action_box.style.width = '100px';
 
-                    div.classList.add('disabled');
-                    div.style.color = '#777';
+		overlay.style.display = "block";
 
-                } else {
+		setTimeout(() => { action_box.style.top = "50px"; }, 0);
+		setTimeout(() => { overlay.style.opacity = "1"; }, 0);
 
-                    div.addEventListener('click', () => {
-                        item.action()
-                        setTimeout(() => { overlay.style.opacity = "0" }, 1);
-                        setTimeout(() => { action_box.style.top = "-100px" }, 1);
-                        setTimeout(() => { action_box.innerHTML = "" }, 100);
-                        setTimeout(() => { overlay.style.display = "none"; }, 500);
-                    })
-                    
-                }
-                
-                action_box.appendChild(div);
+		overlay.addEventListener("click", () => {
+			setTimeout(() => { overlay.style.opacity = "0"; }, 0);
+			setTimeout(() => { overlay.style.display = "none"; }, 100);
+			setTimeout(() => { action_box.style.top = "-500px"; }, 0);
+		});
 
-            });
+		values.forEach((item) => {
+			let div = document.createElement("div");
+			if (item.name == "hr") {
+				div.innerHTML = "<hr>";
+			} else {
+				div.textContent = item.name;
 
-        }
+				if (item.disable) {
+					div.classList.add("disabled");
+					div.style.color = "#777";
+				} else {
+					div.addEventListener("click", () => {
+						setTimeout(() => { overlay.style.opacity = "0"; }, 0);
+						setTimeout(() => { overlay.style.display = "none"; }, 100);
+						setTimeout(() => { action_box.style.top = "-500px"; }, 0);
+						item.action();
+					});
+				}
 
-        overlay.addEventListener('click', () => {
+			}
+			action_box.appendChild(div);
+		});
 
-            setTimeout(() => { overlay.style.opacity = "0" }, 1);
-            setTimeout(() => { action_box.style.top = "-100px" }, 1);
-            setTimeout(() => { action_box.innerHTML = "" }, 100);
-            setTimeout(() => { overlay.style.display = "none"; }, 500);
+	} else {
+		setTimeout(() => {
+			action_box.innerHTML = valueArg;
 
-        })
+			overlay.style.display = "block";
+			action_box.style.width = '300px';
 
-    } else {
+			setTimeout(() => { action_box.style.top = "50px"; }, 0);
+			setTimeout(() => { overlay.style.opacity = "1"; }, 0);
 
-    }
+			overlay.addEventListener("click", () => {
+				setTimeout(() => { overlay.style.opacity = "0"; }, 0);
+				setTimeout(() => { overlay.style.display = "none"; }, 100);
+				setTimeout(() => { action_box.style.top = "-500px"; }, 0);
+			});
 
+			// alert(valueArg)
+		}, 150);
+	}
 }
 
 // header
-const header = document.createElement('header');
-header.classList = "glow f_row"
+const header = document.createElement("header");
+header.classList = "glow f_row";
 header.innerHTML = `
 
-    <h1>Pg</h1>
+    <h1 onclick="console.log('Para :)')">Pg</h1>
 
     <vr></vr>
 
-    <div class="f_col" id="header_top_bar_status">
+    <!--div class="f_col" id="header_top_bar_status">
         <p onclick="alert('File renaming is not available yet')">Unnamed</p>
         <p onclick="alert('File saving is not available yet')">Unsaved document</p>
     </div>
 
-    <vr></vr>
+    <vr></vr-->
 
-    <div id="header_top_bar_actions" style="           ">
-    ` +                                     /* f_col */ `
+    <div id="header_top_bar_actions">
         <div id="header_top_bar_actions_select">
             <p class="action" onclick="show_action(this, 0)">File</p>
-            <p class="action" onclick="show_action(this, 0)">Edit</p>
+            <p class="action" onclick="show_action(this, 0)">Help</p>
+            <!--p class="action" onclick="show_action(this, 0)">Edit</p-->
         </div>
-        <div id="header_top_bar_actions_do">
+        <!--div id="header_top_bar_actions_do">
             <div class="document_action" onclick="show_action(this, 1)" id="action_setfont"><p> Font: <span style="font-family: Serif"> Serif </span> </p> </div>
             <div class="document_action" onclick="show_action(this, 1)" id="action_setsize"><p> Size: 12px </p> </div>
             <div class="document_action" onclick="show_action(this, 1)" onclick="show_action(this, 0)" id="action_setcolorfont"><p> Font <br> </p><div id="action_setcolorfont_box"> </div> </div>
             <div class="document_action" onclick="show_action(this, 1)" id="action_setcolorback"><p> Back <br> </p><div id="action_setcolorback_box"> </div> </div>
-            <div class="document_toggle" id="action_black"><b> N </b> </div>
-            <div class="document_toggle" id="action_italic"><i> K </i> </div>
-            <div class="document_toggle" id="action_underline"><span style="text-decoration: underline;"> U </span> </div>
-            </div>
+            <div class="document_action" id="action_black"><b> N </b> </div>
+            <div class="document_action" id="action_italic"><i> K </i> </div>
+            <div class="document_action" id="action_underline"><span style="text-decoration: underline;"> U </span> </div>
+		</div-->
     </div>
 
-`
+`;
 
-// main 
-const main = document.createElement('main');
-main.classList = "f_col"
+// main
+const main = document.createElement("main");
+main.innerHTML = `
+	<!--div id="sheet" contenteditable="true">
+		Text
+	</div-->
+	<textarea id="sheet" placeholder="Type your text here..." ></textarea>
+`;
 
 // action box
-const action_box = document.createElement('div');
+const action_box = document.createElement("div");
 action_box.id = "action_box";
 
 // hoverbg
-const overlay = document.createElement('div');
+const overlay = document.createElement("div");
 overlay.id = "overlay";
-
 
 // insert values
 document.body.appendChild(action_box);
@@ -190,16 +256,17 @@ document.body.appendChild(overlay);
 document.body.appendChild(header);
 document.body.appendChild(main);
 
-// show page
-setTimeout(() => { document.body.opacity = "0"; }, 500);
-
 // after DOM load
-for (let i = 0; i < buttons_toggle.length; i++) {
-    buttons_toggle[i].addEventListener('click', () => {
-        if (buttons_toggle[i].classList == "document_toggle clicked") {
-            buttons_toggle[i].classList = "document_toggle"
-        } else {
-            buttons_toggle[i].classList = "document_toggle clicked"
-        }
-    })
-}
+document.addEventListener("DOMContentLoaded", () => {
+
+	const sheet = document.getElementById("sheet");
+
+	// sheet.addEventListener("focusin", () => {
+	// 	sheet.textContent = sheet.innerHTML.replace(/<br\s*[/]?>/gi, "\n");
+	// })
+
+	// sheet.addEventListener("focusout", () => {
+	// 	sheet.innerHTML = sheet.textContent.replace(/\n/g, "<b>");
+	// })
+
+})
